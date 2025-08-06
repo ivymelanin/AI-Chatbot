52b1180cbc4ce8eea6f963b4f27836cb1e3f50dd
Lerato Khauta <leratokhauta20@gmail.com>
2025-08-06 15:40:09 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -16,6 +16,24 @@ const createMsgElement = (content, ...classes) => {
     div.innerHTML= content;
     return div;
 }
+
+// Simulate typng effect for bot response
+const typingEffect = (text, textElement, botMsgDiv) => {
+    textElement.textContent = "";
+    const words = text.split(" ");
+    let wordIndex = 0;
+
+    // Set an interval to type each word
+    const typingInterval = setInterval(() =>{
+        if(wordIndex < words.length){
+            textElement.textContent += (wordIndex === 0 ? "": " ") + words[wordIndex++]; 
+            botMsgDiv.classList.remove("loading");
+        } else{
+            clearInterval(typingInterval);
+        }
+    }, 40);
+}
+
  // Make the API call and generate the bot's response
 const generateResponse = async (botMsgDiv) => {
     const textElement = botMsgDiv.querySelector(".message-text");
@@ -35,9 +53,9 @@ const generateResponse = async (botMsgDiv) => {
         const data = await response.json();
         if(!response.ok) throw new Error(data.error.message);
 
-        // Process the response text and display it
+        // Process the response text and display with typing effect
         const responseText = data.candidates[0].content.parts[0].text.replace(/\*\*([^*]+)\*\*/g, "$1").trim();
-        textElement.textContent = responseText;
+        typingEffect(responseText, textElement, botMsgDiv);
     } catch (error){
         console.log(error);
     }
