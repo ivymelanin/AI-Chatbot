9df273b2b43f19c1264a8b64550090565129a647
Lerato Khauta <leratokhauta20@gmail.com>
2025-08-06 13:25:19 +0200

    Update script.js

---
+++ src/script.js
@@ -0,0 +1,14 @@
+const promptForm = document.querySelector(".prompt-form");
+const promptInput = promptForm.querySelector(".prompt-input");
+let userMessage = "";
+
+//Handle the form submission
+const handleFormSubmit = (e) => {
+    e.preventDefault();
+    userMessage = promptInput.value.trim();
+    if(userMessage) return;
+
+    console.log(userMessage);
+}
+
+promptForm.addEventListener("submit", handleFormSubmit);
