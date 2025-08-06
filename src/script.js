3b1777e0daf5d6d976639fc3c2a65ee7b5fd2794
Lerato Khauta <leratokhauta20@gmail.com>
2025-08-06 15:02:42 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,7 +2,12 @@ const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
 
+//API Setup
+const API_KEY = "AIzaSyAQC_qO258b5szQoT6suXJ3erJ6GEqSGwc"
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
+
 let userMessage = "";
+const chatHistory = [];
 
 //function to create message elements
 const createMsgElement = (content, ...classes) => {
@@ -10,6 +15,29 @@ const createMsgElement = (content, ...classes) => {
     div.classList.add("message", ...classes);
     div.innerHTML= content;
     return div;
+}
+ // Make the API call and generate the bot's response
+const generateResponse = async () => {
+    //Add user messages to the chat history
+    chatHistory.push({
+        role: "user",
+        parts: [{ text: userMessage }]
+    });
+    try{
+        // Send the chat history to the API to get a response
+        const response = await fetch(API_URL{
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ contents: chatHistory })
+        });
+
+        const data = await response.json();
+        if(!response.ok) throw new Error(data.error.message);
+
+        console.log(data);
+    } catch (error){
+        console.log(error);
+    }
 }
 
 //Handle the form submission
@@ -31,6 +59,7 @@ const handleFormSubmit = (e) => {
         const botMsgHTML = `<img src="src/media/loading_icon_small_transparent.png" class="icon"><p class="message-text">Just a sec...</p>`;
         const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
         chatsContainer.appendChild(botMsgDiv);
+        generateResponse();
     }, 600);
 }
 
