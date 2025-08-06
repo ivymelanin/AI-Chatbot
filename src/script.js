6ac807aeb56f920a91713a78e43dcacb5c8d5a04
Lerato Khauta <leratokhauta20@gmail.com>
2025-08-06 13:49:11 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,14 +1,28 @@
+const chatsContainer = document.querySelector(".chats-container");
 const promptForm = document.querySelector(".prompt-form");
 const promptInput = promptForm.querySelector(".prompt-input");
+
 let userMessage = "";
+
+//function to create message elements
+const createMsgElement = (content, className) => {
+    const div = document.createElement("div");
+    div.classList.add("message", className);
+    div.innerHTML= content;
+    return div;
+}
 
 //Handle the form submission
 const handleFormSubmit = (e) => {
     e.preventDefault();
     userMessage = promptInput.value.trim();
     if(userMessage) return;
+    // Generate user message HTML and add in the chats container
+    const userMsgHTML = `<p class="message-text"></p>`;
+    const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
 
-    console.log(userMessage);
+    userMsgDiv.querySelector(".message-text").textContent = userMessage;
+    chatsContainer.appendChild(userMsgDiv);
 }
 
 promptForm.addEventListener("submit", handleFormSubmit);
