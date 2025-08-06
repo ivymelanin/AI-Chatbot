a8ef3407a1d167be3f6cbd8e30a067df9359d775
Lerato Khauta <leratokhauta20@gmail.com>
2025-08-06 14:16:08 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,9 +5,9 @@ const promptInput = promptForm.querySelector(".prompt-input");
 let userMessage = "";
 
 //function to create message elements
-const createMsgElement = (content, className) => {
+const createMsgElement = (content, ...classes) => {
     const div = document.createElement("div");
-    div.classList.add("message", className);
+    div.classList.add("message", ...classes);
     div.innerHTML= content;
     return div;
 }
@@ -17,6 +17,8 @@ const handleFormSubmit = (e) => {
     e.preventDefault();
     userMessage = promptInput.value.trim();
     if(!userMessage) return;
+
+    promptInput.value = "";
     // Generate user message HTML and add in the chats container
     const userMsgHTML = `<p class="message-text"></p>`;
     const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
@@ -27,7 +29,7 @@ const handleFormSubmit = (e) => {
     setTimeout(() => {
         // Generate user message HTML and add in the chats container after 600ms
         const botMsgHTML = `<img src="src/media/loading_icon_small_transparent.png" class="icon"><p class="message-text">Just a sec...</p>`;
-        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message");
+        const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
         chatsContainer.appendChild(botMsgDiv);
     }, 600);
 }
